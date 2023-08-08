b3274ab8373be5af6cc7de834cbe1b470588f2dd
zhitenev.andr <zhitenev.andr@gmail.com>
2023-08-08 11:13:23 +0300

    changed contentblock & added vuex

---
+++ src/components/ContentBlock/List.vue
@@ -0,0 +1,35 @@
+<template>
+	<v-subheader
+			v-if="item.header"
+			:key="item.header"
+			v-text="item.header"
+	></v-subheader>
+
+	<v-divider
+			v-else-if="item.divider"
+			:key="index"
+			:inset="item.inset"
+	></v-divider>
+
+	<v-list-item
+			v-else
+			:key="item.title"
+	>
+		<v-list-item-avatar>
+			<v-img :src="item.avatar"></v-img>
+		</v-list-item-avatar>
+
+		<v-list-item-content>
+			<v-list-item-title v-html="item.title"></v-list-item-title>
+			<v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+		</v-list-item-content>
+	</v-list-item>
+</template>
+
+<script>
+	export default {
+		props: {
+			item: Object,
+		}
+	}
+</script>
---
+++ src/components/ContentBlock/Logo.vue
@@ -0,0 +1,8 @@
+<template>
+	<v-img
+		:src="require('../../assets/logo.svg')"
+		class="my-3"
+		contain
+		height="200"
+	/>
+</template>
